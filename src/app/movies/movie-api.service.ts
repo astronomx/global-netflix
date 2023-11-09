d1799226cc7dbf7d67e0b511d375e2c6cf9fc4dd
astronomx <quentinvalen@gmail.com>
2023-11-09 14:57:58 +0100

    Fixed API security issue

--- src/app/movies/movie-api.service.ts
+++ src/app/movies/movie-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class MovieApiService {
     return this.http.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmM4OTJmM2QwODNhOTk4YmE4NzgxNmI2N2E2NjI0NyIsInN1YiI6IjY1NGNiNGRhZDQ2NTM3MDBhYjk0NjU3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vAhOymOLAWhJI_E1BAAFs5EaaX-B-YvNP9PBZ-7duoA'
+        Authorization: `Bearer ${environment.apiKey}`
       }
     })
   }
